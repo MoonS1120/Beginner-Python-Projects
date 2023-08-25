d033797a599bed22114030c66932c59a9bfa76a6
MoonS <99530280+MoonS1120@users.noreply.github.com>
2023-08-25 23:58:39 +0800

    Create binary-search-recursion.py

---
+++ Binary-search-algorithm/binary-search-recursion.py
@@ -0,0 +1,29 @@
+from random import randint
+
+values = []
+for i in range(10):
+    values.append(randint(1, 10))
+target_value = int(input("Target value: "))
+sorted_values = sorted(list(set(values)))
+
+def binary_search(values, target, start_index=0):
+    midpoint = len(values) // 2
+
+    if len(values) == 0:
+        return False
+
+    if values[midpoint] == target:
+        return midpoint + start_index
+    elif values[midpoint] > target:
+        return binary_search(values[:midpoint], target, start_index)
+    else:
+        return binary_search(values[midpoint+1:], target, start_index + midpoint + 1)
+    
+
+if __name__ == "__main__":
+    print(sorted_values)
+    result = binary_search(sorted_values, target_value)
+    if result is False:
+        print("Target value not found.")
+    else:
+        print("Target value found at index:", result)
