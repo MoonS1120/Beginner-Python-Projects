bb2b02154411c2b1113bc43bee1ffba58125f15b
MoonS <99530280+MoonS1120@users.noreply.github.com>
2023-08-26 00:14:14 +0800

    Create binary-search-iteration.py

---
+++ Binary-search-algorithm/binary-search-iteration.py
@@ -0,0 +1,29 @@
+from random import randint
+
+values = []
+for i in range(10):
+    values.append(randint(1, 10))
+target_value = int(input("Target value: "))
+sorted_values = sorted(list(set(values)))
+
+def binary_search(values, target):
+    low = 0
+    high = len(values) - 1
+
+    while low <= high:
+        midpoint = (low + high) // 2
+        if values[midpoint] == target:
+            return midpoint
+        elif values[midpoint] > target:
+            high = midpoint - 1
+        else:
+            low = midpoint + 1
+    return False
+
+if __name__ == "__main__":
+    print(sorted_values)
+    result = binary_search(sorted_values, target_value)
+    if result is False:
+        print("Target value not found.")
+    else:
+        print("Target value found at index:", result)
