c9059ccf1e743b13980b2c52f877315fc851b8c5
MoonS <msuyoung5@gmail.com>
2023-08-29 18:05:18 +0800

    Improvement for readability of code

--- Dice-simulator/dice.py
+++ Dice-simulator/dice.py
@@ -1,80 +1,74 @@
 from random import randint
 
 def main():
-    number_of_dice = int(input("Number of dice: "))
+    num_dice = int(input("Number of dice: "))
+    dice_val = roll_dice(num_dice)
 
-    #Creating a list with 'number_of_dice' numbers of random integer between 1,6 (inclusive)
-    for _ in range(number_of_dice):
-        dice_numbers.append(randint(1,6))
+    horizontal_dice(dice_val)
 
-    horizontal_dice()
-
-    #Totalling
     total = 0
-    for number in dice_numbers:
+    for number in dice_val:
         total += number
     print(f"Total: {total}")
 
+def roll_dice(num_dice):
+    dice_val = [randint(1,6) for _ in range(num_dice)]
+    return dice_val
 
-def vertical_dice():
-    for i in dice_numbers:
+def vertical_dice(dice_val):
+    for i in dice_val:
         print('\n'.join(dice_art[i]))
 
-
-def horizontal_dice():
+def horizontal_dice(dice_val):
     for line in range(5):
-        for number in dice_numbers:
-            print(dice_art.get(number)[line], end='')
+        for num in dice_val:
+            print(dice_art[num-1][line], end='')
         print()
 
-#declaring list
-dice_numbers=[]
+dice_art = [
+       [
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+       ],
+       [
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+       ],
+       [
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+       ],
+       [
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+       ],
+       [
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+       ],
+       [
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+       ]
+]
 
-#ASCII art of dice
-dice_art = {
-    1: (
-        "┌─────────┐",
-        "│         │",
-        "│    ●    │",
-        "│         │",
-        "└─────────┘",
-    ),
-    2: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│         │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    3: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│    ●    │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    4: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│         │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    5: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│    ●    │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    6: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-}
-        
 if __name__ == "__main__":
     main()
