c43888ef5d06bbb8d2b8ed07715b04c407109f8c
MoonS <msuyoung5@gmail.com>
2023-09-06 21:35:20 +0800

    Update currency-converter.py

--- Currency-converter/currency-converter.py
+++ Currency-converter/currency-converter.py
@@ -4,7 +4,7 @@ def convert(from_currency, to_currency, amount, url):
     data = requests.get(url).json()
     rates = data['rates']
     initial_amount = amount
-    amount = round(amount/rates[from_currency]*rates[to_currency], 2)
+    amount = amount/rates[from_currency]*rates[to_currency]
     return initial_amount, amount
 
 def main():
@@ -14,7 +14,7 @@ def main():
     to_c = input("To: ")
     amount = int(input("Amount: "))
     initial_amount, amount = convert(from_c, to_c, amount, url)
-    print(f"{initial_amount} {from_c} is equal to {amount} {to_c}")
+    print(f"{initial_amount} {from_c} is equal to {amount:.2f} {to_c}")
 
 if __name__ == "__main__":
     main()
