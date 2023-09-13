be1f8b0481803a2996e68c54cdea62aee84e6fbd
MoonS <msuyoung5@gmail.com>
2023-09-13 21:34:38 +0800

    Update matrices-ver1.py

--- Simple-matrices-calculator/matrices-ver1.py
+++ Simple-matrices-calculator/matrices-ver1.py
@@ -1,3 +1,5 @@
+import math
+
 A = [[1, 5, -3],
      [2, -3, 4],
      [-2, 4, 2]]
@@ -12,6 +14,9 @@ C = [[-2, 1],
 
 D = [[2, 4],
      [-3, 1]]
+
+E = [[1, 0],
+     [0, 1]]
 
 
 def addition(A, B):
@@ -28,8 +33,10 @@ def multiplication(A, B):
 def determinant(A):
     return A[0][0]*A[1][1] - A[0][1]*A[1][0]
 
-def rotation():
-    ...
+def rotation(A, angle):
+    rotation_matrix = [[math.cos(angle), -math.sin(angle)],
+                       [math.sin(angle), math.cos(angle)]]
+    return multiplication(rotation_matrix, A)
 
 for row in addition(A, B):
     print(row)
@@ -39,4 +46,9 @@ for row in multiplication(A, C):
     print(row)
 print()
 
-print(determinant(D))
+print(determinant(D),'\n')
+
+for row in rotation(E, math.pi/2):
+    row = list(map(lambda x: round(x, ndigits=2), row))
+    print(row)
+print()
